b5c246621a902c36c1b82dbaefec61b2e7c4e706
Piwccle <sergiosergi11@hotmail.com>
2026-06-10 13:09:59 +0200

    fix: update output descriptions for openvidu credentials to correct links

--- pro/elastic/output.tf
+++ pro/elastic/output.tf
@@ -1,4 +1,4 @@
 output "openvidu_credentials" {
-  description = "SSH to the instance to get the credentials. For more information, visit https://openvidu.io/latest/docs/self-hosting/elastic/oracle/install/#check-deployment-outputs-in-the-instance"
-  value       = "SSH to the instance to get the credentials. For more information, visit https://openvidu.io/latest/docs/self-hosting/elastic/oracle/install/#check-deployment-outputs-in-the-instance"
+  description = "SSH to the instance to get the credentials. For more information, visit https://openvidu.io/latest/docs/self-hosting/elastic/oracle/install/#view-openvidu-credentials-in-the-instance"
+  value       = "SSH to the instance to get the credentials. For more information, visit https://openvidu.io/latest/docs/self-hosting/elastic/oracle/install/#view-openvidu-credentials-in-the-instance"
 }
